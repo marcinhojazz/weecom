59dfc23ea0913bc3136636f151be6f872f0f8950
marcinhojazz <marcinhojazz@gmail.com>
2023-08-21 13:12:50 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const PORT = 3000;
+
+// Sevidor de arquivos estáticos: HTML, CSS, JS, imagens...
+app.use(express.static('./'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + 'index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta http://localhost:${PORT}`)
+});
